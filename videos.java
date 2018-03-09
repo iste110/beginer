99b1930b381084c017250c4630276063b08466bd
iste110 <37108024+iste110@users.noreply.github.com>
2018-03-09 17:51:20 +0530

    Create videos.java

---
+++ videos.java
@@ -0,0 +1,44 @@
+{
+ "kind": "youtube#videoListResponse",
+ "etag": "\"UCBpFjp2h75_b92t44sqraUcyu0/sDAlsG9NGKfr6v5AlPZKSEZdtqA\"",
+ "videos": [
+  {
+   "ID": "7lCDEYXw3mM",
+   "Kind": "youtube#video",
+   "Etag": "\"UCBpFjp2h75_b92t44sqraUcyu0/iYynQR8AtacsFUwWmrVaw4Smb_Q\"",
+   "Snippet": {
+    "PublishedAt": "2012-06-20T22:45:24.000Z",
+    "ChannelId": "UC_x5XG1OV2P6uZZ5FSM9Ttw",
+    "Title": "Google I/O 101: Q&A On Using Google APIs",
+    "Description": "Antonio Fuentes speaks to us and takes questions on working with Google APIs and OAuth 2.0.",
+    "Thumbnails": {
+     "Default": {
+      "url": "https://i.ytimg.com/vi/7lCDEYXw3mM/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/7lCDEYXw3mM/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/7lCDEYXw3mM/hqdefault.jpg"
+     }
+    },
+    "categoryId": "28"
+   },
+   "contentDetails": {
+    "duration": "PT15M51S",
+    "aspectRatio": "RATIO_16_9"
+   },
+   "statistics": {
+    "ViewCount": "3057",
+    "LikeCount": "25",
+    "DislikeCount": "0",
+    "FavoriteCount": "17",
+    "CommentCount": "12"
+   },
+   "status": {
+    "uploadStatus": "STATUS_PROCESSED",
+    "privacyStatus": "PRIVACY_PUBLIC"
+   }
+  }
+ ]
+}
